a6d39f4183b2a8f0767b788015fc8e61ac658b23
Bianca B.A <1932648@champlaincollege.qc.ca>
2026-04-10 10:52:23 -0400

    initial commit of School timetabling project

---
+++ src/main/java/org/acme/schooltimetabling/persistence/LessonRepository.java
@@ -0,0 +1,12 @@
+ckage org.acme.schooltimetabling.persistence;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.acme.schooltimetabling.domain.Lesson;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+@ApplicationScoped
+public class LessonRepository implements PanacheRepository<Lesson> {
+
+}
---
+++ src/main/java/org/acme/schooltimetabling/persistence/RoomRepository.java
@@ -0,0 +1,12 @@
+ckage org.acme.schooltimetabling.persistence;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+import org.acme.schooltimetabling.domain.Room;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+@ApplicationScoped
+public class RoomRepository implements PanacheRepository<Room> {
+
+}
